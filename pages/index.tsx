7f486da59f4ebc513bea356002a6b5ac33672fbc
Miguel Penha <miguel.penha1@gmail.com>
2025-03-15 15:39:22 -0300

    Add App section

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Initial from '../components/Initial'
 import Showcases from '../components/Showcases'
 import Features from '../components/Features'
 import About from '../components/About'
+import App from '../components/App'
 import Footer from '../components/Footer'
 
 function Home() {
@@ -16,6 +17,7 @@ function Home() {
             <Showcases/>
             <Features/>
             <About/>
+            <App/>
         </Container>
         <Footer/>
     </>
