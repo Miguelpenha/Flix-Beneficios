f20b724e5eaaeef3647025a683da7e21996b8914
Miguel Penha <miguel.penha1@gmail.com>
2025-03-18 14:17:52 -0300

    Update index.tsx

--- components/ButtonApp/index.tsx
+++ components/ButtonApp/index.tsx
@@ -15,7 +15,7 @@ function ButtonApp() {
                             <path d="M0 0h24v24H0z" fill="none"/>
                             <path d="M19 9h-4V3H9v6H5l7 7 7-7zM5 18v2h14v-2H5z"/>
                         </Icon>
-                        <Text>Baixe agora</Text>
+                        <Text>BAIXE AGORA</Text>
                     </Container>
                 )}
                 {system === 'IOS' && (
@@ -24,7 +24,7 @@ function ButtonApp() {
                             <path d="M0 0h24v24H0z" fill="none"/>
                             <path d="M19 9h-4V3H9v6H5l7 7 7-7zM5 18v2h14v-2H5z"/>
                         </Icon>
-                        <Text>Baixe agora</Text>
+                        <Text>BAIXE AGORA</Text>
                     </Container>
                 )}
                 {system !== 'Other' && (
